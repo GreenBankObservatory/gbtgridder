907800c772578e48a5913b8c509d0686fd18e51d
Thomas Chamberlin <tchamber@nrao.edu>
2021-08-19 14:45:16 +0200

    Fixed

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@
 
 # Always prefer setuptools over distutils
 import pathlib
+
 from setuptools import find_packages, setup
+
 here = pathlib.Path(__file__).parent.resolve()
+
 # Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
-
-
-
-
-
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
