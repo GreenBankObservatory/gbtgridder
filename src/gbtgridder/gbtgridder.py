24f31db29d700aa447a7d9d96f17d31d0089edb3
Kathlyn Purcell <kpurcell@belinda.gb.nrao.edu>
2023-03-08 13:44:20 -0500

    in-release bug fig to allow for the running of clonecube arg

--- src/gbtgridder/gbtgridder.py
+++ src/gbtgridder/gbtgridder.py
@@ -33,6 +33,7 @@ from . import gbtgridder_args
 from .get_data import get_data
 from .grid_otf import grid_otf
 from .make_header import make_header
+from .get_cube_info import get_cube_info
 
 gbtgridderVersion = "2.0"
 _C = 299792458.0  # speed of light (m/s)
